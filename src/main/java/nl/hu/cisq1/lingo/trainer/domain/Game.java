52f12f2e687deb08b0dc681256187a1ca26695ca
Ayakk <57104499+Ayakk@users.noreply.github.com>
2021-10-04 10:13:05 +0200

    setup game.java

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Game.java
@@ -1,4 +1,35 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import nl.hu.cisq1.lingo.words.domain.Word;
+
 public class Game {
+    private Feedback f;
+    private Round r;
+    private Word w;
+
+    public Feedback getF() {
+        return f;
+    }
+
+    public void setF(Feedback f) {
+        this.f = f;
+    }
+
+    public Round getR() {
+        return r;
+    }
+
+    public void setR(Round r) {
+        this.r = r;
+    }
+
+    public Word getW() {
+        return w;
+    }
+
+    public void setW(Word w) {
+        this.w = w;
+    }
+
+    
 }
