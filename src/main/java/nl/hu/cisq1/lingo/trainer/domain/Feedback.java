8ea4e673e8e0a6a07cd493d58de02ecfee365e7e
Ayakk <57104499+Ayakk@users.noreply.github.com>
2021-10-06 16:57:05 +0200

    removed redundant code

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -1,7 +1,6 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
 import nl.hu.cisq1.lingo.words.domain.Word;
-import nl.hu.cisq1.lingo.words.domain.exception.InvalidFeedbackException;
 
 import java.util.*;
 
@@ -10,32 +9,29 @@ public class Feedback {
     private List<Mark> markList;
     private HashMap<Integer, String> hintList = new HashMap<Integer, String>();
     private Word w;
+    private String wordToGuess;
 
-    public Feedback(){
+    public Feedback() {
 
     }
 
     public Feedback(String attempt) {
         this.attempt = attempt;
-        w = new Word();
+    }
+
+    public Feedback(String attempt, String GuessWord) {
+        this.attempt = attempt;
+        this.wordToGuess = GuessWord;
     }
 
     public Feedback(String attempt, List<Mark> markList) {
         this.attempt = attempt;
+        wordToGuess="woord";
         this.markList = markList;
     }
 
-
     public String getAttempt() {
         return attempt;
-    }
-
-    public Word getW() {
-        return w;
-    }
-
-    public void setW(Word w) {
-        this.w = w;
     }
 
     public void setAttempt(String attempt) {
@@ -50,70 +46,69 @@ public class Feedback {
         this.markList = markList;
     }
 
-    public boolean isWordGuessed(Feedback feedback){
-        try{ //TODO geen try catch nodig--> check opdracht named constructor
-            //is Guessed starts on true
-            boolean isGuessed = true;
-            //for loop checks if ifGuessed should be put to false
-            for(Mark m : feedback.getMarkList()){
-                //checks if all marks are correct, if not isGuessed = false
-                if (!m.equals(Mark.CORRECT)){
-                    isGuessed = false;
-                }
+    public boolean isWordGuessed(Feedback feedback) {
+        //is Guessed starts on true
+        boolean isGuessed = true;
+        //for loop checks if ifGuessed should be put to false
+        for (Mark m : feedback.getMarkList()) {
+            //checks if all marks are correct, if not isGuessed = false
+            if (!m.equals(Mark.CORRECT)) {
+                isGuessed = false;
             }
-            return isGuessed;
-        }catch (InvalidFeedbackException exception){
-            throw new RuntimeException(exception.getMessage());
         }
+        return isGuessed;
     }
 
-    public boolean wordIsInvalid(Feedback feedback){
+
+    public boolean wordIsInvalid(Feedback feedback) {
         boolean isInvalid = false;
         String word = feedback.getAttempt();
         //if the word length is outside what is allowed isInvalid will become true
-        if (word.length() < 4 || word.length() > 6){
+        if (word.length() < 4 || word.length() > 6) {
             isInvalid = true;
         }
         return isInvalid;
     }
 
-    public String giveBetterHint(){
+    public String giveBetterHint() {
         HashMap<Integer, Character> guessHolder = new HashMap<Integer, Character>();
         HashMap<Integer, Character> attemptHolder = new HashMap<Integer, Character>();
         List<Mark> markL = new ArrayList<>();
 
-        String wordToGuess = "woord";
+        if (wordToGuess == null){
+            wordToGuess="woord";
+        }
         System.out.println("Attempt: " + getAttempt());
         System.out.println("ToGuessWord: " + wordToGuess);
 
         String returnString = "";
 
-        for (int i = 0; i < getAttempt().length(); i++){
+        for (int i = 0; i < getAttempt().length(); i++) {
             char c = getAttempt().charAt(i);
             //Process char
             guessHolder.put(i, c);
         }
 
-        for (int i = 0; i < wordToGuess.length(); i++){
+        for (int i = 0; i < wordToGuess.length(); i++) {
             char c = wordToGuess.charAt(i);
             //Process char
             attemptHolder.put(i, c);
         }
 
-        if (guessHolder.equals(attemptHolder)){
-            for (int i = 0; i < guessHolder.size(); i++){
+        if (guessHolder.equals(attemptHolder)) {
+            for (int i = 0; i < guessHolder.size(); i++) {
                 markL.add(Mark.CORRECT);
             }
-        } else if (guessHolder.size() != attemptHolder.size()){
+        } else if (guessHolder.size() != attemptHolder.size()) {
             for (int i = 0; i < guessHolder.size(); i++)
                 markL.add(Mark.INVALID);
-        }else {
-            for (int i = 0; i < guessHolder.size(); i++){
-                if (attemptHolder.get(i).equals(guessHolder.get(i))){
+        } else {
+            for (int i = 0; i < guessHolder.size(); i++) {
+                if (attemptHolder.get(i).equals(guessHolder.get(i))) {
                     markL.add(Mark.CORRECT);
-                } else if (attemptHolder.containsValue(guessHolder.get(i))){
+                } else if (attemptHolder.containsValue(guessHolder.get(i))) {
                     markL.add(Mark.PRESENT);
-                } else{
+                } else {
                     markL.add(Mark.ABSENT);
                 }
             }
@@ -126,7 +121,7 @@ public class Feedback {
     }
 
     //convert list of feedback to string with hint
-    public String markListToString(List<Mark> markL, HashMap<Integer, Character> guessHolder){
+    public String markListToString(List<Mark> markL, HashMap<Integer, Character> guessHolder) {
         String returnString = "";
         for (int i = 0; i < markL.size(); i++) {
             if (markL.get(i).equals(Mark.CORRECT)) {
@@ -140,27 +135,5 @@ public class Feedback {
             }
         }
         return returnString;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Feedback feedback = (Feedback) o;
-        return Objects.equals(attempt, feedback.attempt) &&
-                Objects.equals(markList, feedback.markList);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(attempt, markList);
-    }
-
-    @Override
-    public String toString() {
-        return "Feedback{" +
-                "attempt='" + attempt + '\'' +
-                ", markList=" + markList +
-                '}';
     }
 }
