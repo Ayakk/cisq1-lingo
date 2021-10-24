ef0512398de2876fb05b92d64497a1f6d5a43935
Ayakk <57104499+Ayakk@users.noreply.github.com>
2021-10-24 18:54:41 +0200

    CI added and implemented first test

---
+++ src/test/java/nl/hu/cisq1/lingo/trainer/presentation/ControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package nl.hu.cisq1.lingo.trainer.presentation;
+
+import nl.hu.cisq1.lingo.CiTestConfiguration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@Import(CiTestConfiguration.class)
+@AutoConfigureMockMvc
+public class ControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Test if starting new game is successful")
+    void TestStartNewGame(){
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/startgame"))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
